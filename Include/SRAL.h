3258bbf3980ba3deabc210b65d94dfca8b9cc097
unknown <georgijbondarenko248@gmail.com>
2024-08-14 15:06:43 +0300

    Added SRAL API prototype.

--- Include/SRAL.h
+++ Include/SRAL.h
@@ -1,5 +1,21 @@
-#ifndef SRAL_INCLUDED
-#define SRAL_INCLUDED
+#ifndef SRAL_H_
+#define SRAL_H_
 #pragma once
-
+#ifdef _WIN32
+#if defined SRAL_EXPORT
+#define SRAL_API __declspec(dllexport)
+#else
+#define SRAL_API __declspec(dllimport)
 #endif
+#else
+#define SRAL_API
+#endif
+#include <stdbool.h>
+#include <stdlib.h>
+extern "C" SRAL_API bool SRAL_Speak(const char* text, bool interrupt);
+extern "C" SRAL_API bool SRAL_Braille(const char* text);
+extern "C" SRAL_API bool SRAL_Output(const char* text, bool interrupt);
+extern "C" SRAL_API bool SRAL_StopSpeech(void);
+extern "C" SRAL_API int SRAL_GetCurrentScreenReader(void);
+
+#endif // SRAL_H_
---
+++ SRC/SRAL.cpp
@@ -0,0 +1,1 @@
+#include "../Include/SRAL.h"
