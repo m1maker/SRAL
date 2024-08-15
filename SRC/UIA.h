56d479bb20c5db41e47cad6b39bbba5fb0fbdbc2
unknown <georgijbondarenko248@gmail.com>
2024-08-15 18:00:19 +0300

    Win32: Add UIAProvider output.

---
+++ SRC/UIA.h
@@ -0,0 +1,26 @@
+#ifndef UIA_H_
+#define UIA_H_
+#pragma once
+#include "../Include/SRAL.h"
+#include "ScreenReader.h"
+class UIA : public ScreenReader {
+public:
+	bool Speak(const char* text, bool interrupt);
+	bool Braille(const char* text) { return false; }
+	bool StopSpeech();
+	int GetNumber() {
+		return SCREEN_READER_UIA;
+	}
+	bool GetActive();
+	bool Initialize();
+	bool Uninitialize();
+	int GetFeatures() {
+		return SUPPORTS_SPEECH;
+	}
+	void SetVolume(uint64_t) { return; }
+	uint64_t GetVolume() { return 0; }
+	void SetRate(uint64_t) { return; }
+	uint64_t GetRate() { return 0; }
+
+};
+#endif
