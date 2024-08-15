674a1f3fd6104921ae4ea74a0b0b17acd7f99d90
unknown <georgijbondarenko248@gmail.com>
2024-08-15 13:50:08 +0300

    Add supporting for Jaws.

---
+++ SRC/Jaws.h
@@ -0,0 +1,29 @@
+#ifndef JAWS_H_
+#define JAWS_H_
+#pragma once
+#include "../Dep/fsapi.h"
+#include "../Include/SRAL.h"
+#include "ScreenReader.h"
+#include <Windows.h>
+class Jaws : public ScreenReader {
+public:
+	bool Speak(const char* text, bool interrupt);
+	bool Braille(const char* text);
+	bool StopSpeech();
+	int GetNumber() {
+		return SCREEN_READER_JAWS;
+	}
+	bool GetActive();
+	bool Initialize();
+	bool Uninitialize();
+	int GetFeatures() {
+		return SUPPORTS_SPEECH | SUPPORTS_BRAILLE;
+	}
+	void SetVolume(uint64_t) { return; }
+	uint64_t GetVolume() { return 0; }
+	void SetRate(uint64_t) { return; }
+	uint64_t GetRate() { return 0; }
+private:
+	IJawsApi* JawsAPI = nullptr;
+};
+#endif
