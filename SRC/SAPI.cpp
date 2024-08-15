925db19019201135c376bb8bb09b577eed08423c
unknown <georgijbondarenko248@gmail.com>
2024-08-15 12:57:50 +0300

    Added SRAL SAPI interrupt and not interrupt conditions

--- SRC/SAPI.cpp
+++ SRC/SAPI.cpp
@@ -1,4 +1,5 @@
 #include "SAPI.h"
+#include <thread>
 // This function is taken from [NVGT](https://github.com/samtupy/nvgt)
 static char* minitrim(char* data, unsigned long* bufsize, int bitrate, int channels) {
 	char* ptr = data;
@@ -42,32 +43,58 @@ bool SAPI::GetActive() {
 	return instance != nullptr;
 }
 bool SAPI::Speak(const char* text, bool interrupt) {
-	if (instance == nullptr)return false;
+	if (instance == nullptr)
+		return false;
+
 	unsigned long bytes;
 	char* audio_ptr = blastspeak_speak_to_memory(instance, &bytes, text);
-	if (audio_ptr == nullptr)return false;
+	if (audio_ptr == nullptr)
+		return false;
+
 	char* final = minitrim(audio_ptr, &bytes, instance->bits_per_sample, instance->channels);
-	if (final == nullptr) return false;
+	if (final == nullptr)
+		return false;
+
 	if (m_bufferInitialized) {
 		ma_audio_buffer_uninit(&m_buffer);
 		m_bufferInitialized = false;
 	}
+
 	ma_audio_buffer_config bufferConfig = ma_audio_buffer_config_init(ma_format_s16, instance->channels, bytes / 2, (const void*)final, nullptr);
 	bufferConfig.sampleRate = 16000;
 	bufferConfig.channels = 1;
 	ma_result result = ma_audio_buffer_init(&bufferConfig, &m_buffer);
-	if (result != MA_SUCCESS)return false;
+	if (result != MA_SUCCESS)
+		return false;
 	m_bufferInitialized = true;
-	if (m_soundInitialized) {
-		ma_sound_uninit(&m_sound);
-		m_soundInitialized = false;
-	}
 
-	result = ma_sound_init_from_data_source(&m_audioEngine, &m_buffer, 0, nullptr, &m_sound);
-	if (result != MA_SUCCESS)return false;
-	m_soundInitialized = true;
-	return ma_sound_start(&m_sound) == MA_SUCCESS;
+	std::unique_ptr<std::thread> t(new std::thread([this, interrupt]() {
+		if (!interrupt) {
+			if (m_soundInitialized) {
+				while (ma_sound_is_playing(&m_sound) == MA_TRUE) {
+				}
+				ma_sound_uninit(&m_sound);
+				m_soundInitialized = false;
+			}
+		}
+		else {
+			if (m_soundInitialized) {
+				ma_sound_uninit(&m_sound);
+				m_soundInitialized = false;
+			}
+
+		}
+		ma_result res = ma_sound_init_from_data_source(&m_audioEngine, &m_buffer, 0, nullptr, &m_sound);
+		if (res != MA_SUCCESS)
+			return;
+		m_soundInitialized = true;
+		ma_sound_start(&m_sound);
+		}));
+
+	t->join();
+	return true;
 }
+
 bool SAPI::StopSpeech() {
 	if (m_soundInitialized)
 		return ma_sound_stop(&m_sound) == MA_SUCCESS;
