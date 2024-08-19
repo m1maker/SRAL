3bc1feee763d72fa85c55c23d08f819b3ed1cd1c
unknown <georgijbondarenko248@gmail.com>
2024-08-19 17:39:33 +0300

    UIAutomationEngine optimizations and fixes.

--- SRC/UIA.h
+++ SRC/UIA.h
@@ -1,8 +1,11 @@
 #ifndef UIA_H_
 #define UIA_H_
 #pragma once
+#include "../Dep/UIAProvider.h"
 #include "../Include/SRAL.h"
 #include "Engine.h"
+
+
 class UIA : public Engine {
 public:
 	bool Speak(const char* text, bool interrupt);
@@ -23,5 +26,11 @@ public:
 	uint64_t GetRate() { return 0; }
 private:
 	int val = 0;
+private:
+	IUIAutomation* pAutomation = nullptr;
+	IUIAutomationCondition* pCondition = nullptr;
+	VARIANT varName;
+	Provider* pProvider = nullptr;
+	IUIAutomationElement* pElement = nullptr;
 };
 #endif
