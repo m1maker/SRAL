077bb4025bf93b53359b15192ca1ff4ee3aad4f0
unknown <georgijbondarenko248@gmail.com>
2024-08-19 17:39:33 +0300

    UIAutomationEngine optimizations and fixes.

--- SRC/UIA.cpp
+++ SRC/UIA.cpp
@@ -1,50 +1,51 @@
-#include "../Dep/UIAProvider.h"
 #include "Encoding.h"
 #include "UIA.h"
 #include <string>
-static void WINAPI SendNotification(const std::wstring& message, NotificationProcessing flags) {
+
+
+
+bool UIA::Initialize() {
 	CoInitialize(NULL);
-
-	IUIAutomation* pAutomation = NULL;
 	HRESULT hr = CoCreateInstance(CLSID_CUIAutomation, NULL, CLSCTX_INPROC_SERVER, IID_IUIAutomation, (void**)&pAutomation);
-
-	if (SUCCEEDED(hr)) {
-		IUIAutomationCondition* pCondition = NULL;
-		VARIANT varName;
-		varName.vt = VT_BSTR;
-		varName.bstrVal = SysAllocString(L"");
-		hr = pAutomation->CreatePropertyConditionEx(UIA_NamePropertyId, varName, PropertyConditionFlags_None, &pCondition);
-
-		if (SUCCEEDED(hr)) {
-			Provider* pProvider = new Provider(GetForegroundWindow());
-			IUIAutomationElement* pElement = NULL;
-			hr = pAutomation->ElementFromHandle(GetForegroundWindow(), &pElement);
-
-			if (SUCCEEDED(hr)) {
-				hr = UiaRaiseNotificationEvent(pProvider, NotificationKind_ActionCompleted, flags, SysAllocString(message.c_str()), SysAllocString(L""));
-
-				if (SUCCEEDED(hr)) {
-					pProvider->Release();
-				}
-			}
-
-			pCondition->Release();
-		}
-
-		pAutomation->Release();
+	if (FAILED(hr)) {
+		return false;
 	}
 
-	CoUninitialize();
+	varName.vt = VT_BSTR;
+	varName.bstrVal = SysAllocString(L"");
+	hr = pAutomation->CreatePropertyConditionEx(UIA_NamePropertyId, varName, PropertyConditionFlags_None, &pCondition);
+	if (FAILED(hr)) {
+		return false;
+	}
+	return true;
 }
-bool UIA::Initialize() { return true; }
-bool UIA::Uninitialize() { return true; }
+
+bool UIA::Uninitialize() {
+	pProvider->Release();
+
+	pCondition->Release();
+
+	pAutomation->Release();
+
+	return true;
+}
 bool UIA::Speak(const char* text, bool interrupt) {
-	NotificationProcessing flags = NotificationProcessing_All;
+	NotificationProcessing flags = NotificationProcessing_ImportantAll;
 	if (interrupt)
-		flags = NotificationProcessing_ImportantAll;
+		flags = NotificationProcessing_ImportantMostRecent;
 	std::wstring str;
 	UnicodeConvert(text, str);
-	SendNotification(str, flags);
+	pProvider = new Provider(GetForegroundWindow());
+	HRESULT hr = pAutomation->ElementFromHandle(GetForegroundWindow(), &pElement);
+
+	if (FAILED(hr)) {
+		return false;
+	}
+	hr = UiaRaiseNotificationEvent(pProvider, NotificationKind_ActionCompleted, flags, SysAllocString(str.c_str()), SysAllocString(L""));
+	if (FAILED(hr)) {
+		return false;
+	}
+
 	return true;
 
 }
