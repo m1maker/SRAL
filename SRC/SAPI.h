376e1c562f69320af2a8ee3eac0030b008a16d29
unknown <georgijbondarenko248@gmail.com>
2024-08-20 14:43:43 +0300

    Code optimizations.

--- SRC/SAPI.h
+++ SRC/SAPI.h
@@ -8,22 +8,22 @@
 #include "Engine.h"
 class SAPI : public Engine {
 public:
-	bool Speak(const char* text, bool interrupt);
-	bool Braille(const char* text) { return false; }
-	bool StopSpeech();
-	int GetNumber() {
+	bool Speak(const char* text, bool interrupt)override;
+	bool Braille(const char* text)override { return false; }
+	bool StopSpeech()override;
+	int GetNumber()override {
 		return ENGINE_SAPI;
 	}
-	bool GetActive();
-	bool Initialize();
-	bool Uninitialize();
-	int GetFeatures() {
+	bool GetActive()override;
+	bool Initialize()override;
+	bool Uninitialize()override;
+	int GetFeatures()override {
 		return SUPPORTS_SPEECH | SUPPORTS_SPEECH_RATE | SUPPORTS_SPEECH_VOLUME;
 	}
-	void SetVolume(uint64_t value);
-	uint64_t GetVolume();
-	void SetRate(uint64_t value);
-	uint64_t GetRate();
+	void SetVolume(uint64_t value)override;
+	uint64_t GetVolume()override;
+	void SetRate(uint64_t value)override;
+	uint64_t GetRate()override;
 private:
 	blastspeak* instance = nullptr;
 	WAVEFORMATEX wfx;
--- SRC/UIA.h
+++ SRC/UIA.h
@@ -8,24 +8,22 @@
 
 class UIA : public Engine {
 public:
-	bool Speak(const char* text, bool interrupt);
-	bool Braille(const char* text) { return false; }
-	bool StopSpeech();
-	int GetNumber() {
+	bool Speak(const char* text, bool interrupt)override;
+	bool Braille(const char* text)override { return false; }
+	bool StopSpeech()override;
+	int GetNumber()override {
 		return ENGINE_UIA;
 	}
-	bool GetActive();
-	bool Initialize();
-	bool Uninitialize();
-	int GetFeatures() {
+	bool GetActive()override;
+	bool Initialize()override;
+	bool Uninitialize()override;
+	int GetFeatures()override {
 		return SUPPORTS_SPEECH;
 	}
-	void SetVolume(uint64_t) { return; }
-	uint64_t GetVolume() { return 0; }
-	void SetRate(uint64_t) { return; }
-	uint64_t GetRate() { return 0; }
-private:
-	int val = 0;
+	void SetVolume(uint64_t)override { return; }
+	uint64_t GetVolume()override { return 0; }
+	void SetRate(uint64_t)override { return; }
+	uint64_t GetRate()override { return 0; }
 private:
 	IUIAutomation* pAutomation = nullptr;
 	IUIAutomationCondition* pCondition = nullptr;
