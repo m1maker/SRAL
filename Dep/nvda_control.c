c0c72ad0aece0c8e198446de76ef429df9b801e7
m1maker <georgijbondarenko248@gmail.com>
2025-02-09 12:14:07 +0300

    NVDAControlEx: Remove using of hPipe in function arguments. Use the static HANDLE instead.

--- Dep/nvda_control.c
+++ Dep/nvda_control.c
@@ -4,10 +4,12 @@
 extern "C" {
 #endif
 
+static HANDLE g_hNvda;
+
 
 // Connects to the NVDA named pipe
-HANDLE nvda_connect() {
-    HANDLE hPipe = CreateFileW(
+int nvda_connect(void) {
+    g_hNvda = CreateFileW(
         NVDA_PIPE_NAME,          // Pipe name
         GENERIC_READ | GENERIC_WRITE, // Read and write access
         0,                       // No sharing
@@ -16,28 +18,28 @@ HANDLE nvda_connect() {
         0,                       // Default attributes
         NULL                     // No template file
     );
-    return hPipe;
+    return g_hNvda != INVALID_HANDLE_VALUE ? 0 : -1;
 }
 
 // Disconnects from the NVDA named pipe
-void nvda_disconnect(HANDLE hPipe) {
-    if (hPipe != INVALID_HANDLE_VALUE) {
-        CloseHandle(hPipe);
+void nvda_disconnect(void) {
+    if (g_hNvda != INVALID_HANDLE_VALUE) {
+        CloseHandle(g_hNvda);
     }
 }
 
 // Sends a command to the NVDA named pipe
-int nvda_send_command(HANDLE hPipe, const char* command) {
-    if (hPipe == INVALID_HANDLE_VALUE) {
-        hPipe = nvda_connect();  // Reconnect if the pipe is invalid
-        if (hPipe == INVALID_HANDLE_VALUE) {
+int nvda_send_command(const char* command) {
+    if (g_hNvda == INVALID_HANDLE_VALUE) {
+        nvda_connect();  // Reconnect if the pipe is invalid
+        if (g_hNvda == INVALID_HANDLE_VALUE) {
             return -1;
         }
     }
 
     DWORD bytesWritten;
     BOOL result = WriteFile(
-        hPipe,                   // Pipe handle
+        g_hNvda,                   // Pipe handle
         command,                 // Command to send
         (DWORD)strlen(command),  // Length of command
         &bytesWritten,           // Bytes written
@@ -46,60 +48,61 @@ int nvda_send_command(HANDLE hPipe, const char* command) {
 
     if (!result) {
         // If the write fails, close the pipe and reconnect
-        nvda_disconnect(hPipe);
-        hPipe = nvda_connect();
-        if (hPipe == INVALID_HANDLE_VALUE) {
+        nvda_disconnect();
+        nvda_connect();
+        if (g_hNvda == INVALID_HANDLE_VALUE) {
             return -1;
         }
-        return nvda_send_command(hPipe, command);
+        return nvda_send_command(command);
     }
 
     return 0;
 }
 
 // Sends a "speak" command to NVDA
-int nvda_speak(HANDLE hPipe, const char* text, int symbol_level) {
+int nvda_speak(const char* text, int symbol_level) {
     char command[64000];
     snprintf(command, sizeof(command), "speak \"%s\" 0 %d", text, symbol_level);
-    return nvda_send_command(hPipe, command);
+    return nvda_send_command(command);
 }
 
 // Sends a "speakSpelling" command to NVDA
-int nvda_speak_spelling(HANDLE hPipe, const char* text, const char* locale, int use_character_descriptions) {
+int nvda_speak_spelling(const char* text, const char* locale, int use_character_descriptions) {
     char command[64000];
     snprintf(command, sizeof(command), "speakSpelling \"%s\" \"%s\" %d", text, locale, use_character_descriptions);
-    return nvda_send_command(hPipe, command);
+    return nvda_send_command(command);
 }
 
 // Sends a "speakSsml" command to NVDA
-int nvda_speak_ssml(HANDLE hPipe, const char* ssml, int symbol_level) {
+int nvda_speak_ssml(const char* ssml, int symbol_level) {
     char command[64000];
     snprintf(command, sizeof(command), "speakSsml \"%s\" 0 %d", ssml, symbol_level);
-    return nvda_send_command(hPipe, command);
+    return nvda_send_command(command);
 }
 
 // Sends a "pauseSpeech" command to NVDA
-int nvda_pause_speech(HANDLE hPipe, int pause) {
+int nvda_pause_speech(int pause) {
     char command[256];
     snprintf(command, sizeof(command), "pauseSpeech %d", pause);
-    return nvda_send_command(hPipe, command);
+    return nvda_send_command(command);
 }
 
 // Sends a "cancelSpeech" command to NVDA
-int nvda_cancel_speech(HANDLE hPipe) {
-    return nvda_send_command(hPipe, "cancelSpeech");
+int nvda_cancel_speech(void) {
+    return nvda_send_command("cancelSpeech");
 }
 
 // Sends a "braille" command to NVDA
-int nvda_braille(HANDLE hPipe, const char* text) {
+int nvda_braille(const char* text) {
     char command[64000];
     snprintf(command, sizeof(command), "braille \"%s\"", text);
-    return nvda_send_command(hPipe, command);
+    return nvda_send_command(command);
 }
 
 // Sends an "active" command to NVDA
-int nvda_active(HANDLE hPipe) {
-    return nvda_send_command(hPipe, "active");
+int nvda_active(void) {
+    nvda_disconnect();
+    return nvda_send_command("active");
 }
 
 #ifdef __cplusplus
