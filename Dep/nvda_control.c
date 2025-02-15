42f6a3f3e0e700ae93f9b07db48ae9686391a975
m1maker <georgijbondarenko248@gmail.com>
2025-02-15 14:36:01 +0300

    NVDAControlEx: Escape quotes in the text.

--- Dep/nvda_control.c
+++ Dep/nvda_control.c
@@ -1,3 +1,9 @@
+/*
+ * NVDA Control Source File
+ *
+ * Copyright (c) 2025 [m1maker]
+ */
+
 #include "nvda_control.h"
 
 #ifdef __cplusplus
@@ -5,6 +11,24 @@ extern "C" {
 #endif
 
 static HANDLE g_hNvda = INVALID_HANDLE_VALUE;
+
+
+// Function to escape quotes in the text
+static void escape_quotes(const char* input, char* output, size_t output_size) {
+    size_t i, j = 0;
+    for (i = 0; input[i] != '\0' && j < output_size - 1; i++) {
+        if (input[i] == '"') {
+            // Escape the quote
+            if (j < output_size - 2) { // Check if there's space for escaped quote
+                output[j++] = '\\';
+                output[j++] = '"';
+            }
+        } else {
+            output[j++] = input[i];
+        }
+    }
+    output[j] = '\0'; // Null-terminate the output string
+}
 
 
 // Connects to the NVDA named pipe
@@ -67,22 +91,34 @@ int nvda_send_command(const char* command) {
 
 // Sends a "speak" command to NVDA
 int nvda_speak(const char* text, int symbol_level) {
+    char escaped_text[64000];
+    escape_quotes(text, escaped_text, sizeof(escaped_text));
+
+
     char command[64000];
-    snprintf(command, sizeof(command), "speak \"%s\" 0 %d", text, symbol_level);
+    snprintf(command, sizeof(command), "speak \"%s\" 0 %d", escaped_text, symbol_level);
     return nvda_send_command(command);
 }
 
 // Sends a "speakSpelling" command to NVDA
 int nvda_speak_spelling(const char* text, const char* locale, int use_character_descriptions) {
+    char escaped_text[64000];
+    escape_quotes(text, escaped_text, sizeof(escaped_text));
+
+
     char command[64000];
-    snprintf(command, sizeof(command), "speakSpelling \"%s\" \"%s\" %d", text, locale, use_character_descriptions);
+    snprintf(command, sizeof(command), "speakSpelling \"%s\" \"%s\" %d", escaped_text, locale, use_character_descriptions);
     return nvda_send_command(command);
 }
 
 // Sends a "speakSsml" command to NVDA
 int nvda_speak_ssml(const char* ssml, int symbol_level) {
+    char escaped_text[64000];
+    escape_quotes(ssml, escaped_text, sizeof(escaped_text));
+
+
     char command[64000];
-    snprintf(command, sizeof(command), "speakSsml \"%s\" 0 %d", ssml, symbol_level);
+    snprintf(command, sizeof(command), "speakSsml \"%s\" 0 %d", escaped_text, symbol_level);
     return nvda_send_command(command);
 }
 
@@ -100,8 +136,12 @@ int nvda_cancel_speech(void) {
 
 // Sends a "braille" command to NVDA
 int nvda_braille(const char* text) {
+    char escaped_text[64000];
+    escape_quotes(text, escaped_text, sizeof(escaped_text));
+
+
     char command[64000];
-    snprintf(command, sizeof(command), "braille \"%s\"", text);
+    snprintf(command, sizeof(command), "braille \"%s\"", escaped_text);
     return nvda_send_command(command);
 }
 
